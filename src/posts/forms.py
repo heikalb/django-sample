c61db283e78778ab5afd9a36a202f76f5f681268
heikalb <heikal93@gmail.com>
2020-01-03 22:04:39 -0800

    worked on forms

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -10,7 +11,16 @@ class PostForm(forms.ModelForm):
             'award'
         ]
 
+
 class RawPostForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    award = forms.DecimalField()
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Your title'}))
+    description = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': 10, 'class': 'description', 'id': 'meep', 'cols': 100}))
+    award = forms.DecimalField(required=True, initial=0.00, label='Award')
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        if len(title) <= 1:
+            raise forms.ValidationError('Title too short')
+
+        return title
