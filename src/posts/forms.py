4818fd077f7c69ec5936378b28aa7b8904758b95
heikalb <heikal93@gmail.com>
2020-01-09 12:18:51 -0800

    added forms

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -12,6 +12,48 @@ class PostForm(forms.ModelForm):
         ]
 
 
+class BaseForm(forms.Form):
+    title = forms.CharField(required=True,
+                            widget=forms.TextInput(
+                                        attrs={'placeholder': 'Your title'})
+                            )
+
+    description = forms.CharField(required=True,
+                                  widget=forms.Textarea(
+                                            attrs={'rows': 10,
+                                                   'class': 'description',
+                                                   'cols': 100})
+                                  )
+
+    contact_1 = forms.CharField(label='Contact 1', required=True,
+                                widget=forms.TextInput(
+                                            attrs={'placeholder': 'phone or email'}
+                                        )
+                                )
+
+    contact_2 = forms.CharField(label='Contact 2', required=True,
+                                widget=forms.TextInput(
+                                            attrs={'placeholder': 'alternative phone or email'}
+                                        )
+                                )
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        if len(title) <= 3:
+            raise forms.ValidationError('Title too short')
+
+        return title
+
+
+class LostForm(BaseForm):
+    pass
+
+
+class FoundForm(BaseForm):
+    pass
+
+"""
 class RawPostForm(forms.Form):
     title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Your title'}))
     description = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': 10, 'class': 'description', 'id': 'meep', 'cols': 100}))
@@ -24,3 +66,4 @@ class RawPostForm(forms.Form):
             raise forms.ValidationError('Title too short')
 
         return title
+"""
