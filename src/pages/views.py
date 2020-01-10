402f7f4c15ff7f94c83c61d131ead9b56cb0f96b
heikalb <heikal93@gmail.com>
2020-01-10 14:24:53 -0800

    changed post type

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from posts.models import Post
+from posts.models import LostPost
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
-    print(request.user)
-
-    queryset = Post.objects.all()
-
+    queryset = LostPost.objects.all()
     context = {'posts': queryset}
 
     return render(request, 'home.html', context)
