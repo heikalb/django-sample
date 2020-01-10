3badb388020ad7a9afd1e62a10cc45c3d22e690d
heikalb <heikal93@gmail.com>
2020-01-09 16:18:35 -0800

    displayed posts

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from posts.models import Post
 # Create your views here.
+
+
 def home_view(request, *args, **kwargs):
     print(request.user)
 
-    context = {
-        'text': 'Hello, I am a text',
-        'number': 24,
-        'birds': ['sparrows', 'juncos', 'hummingbirds'],
-        'do_display': False
-    }
+    queryset = Post.objects.all()
+
+    context = {'posts': queryset}
 
     return render(request, 'home.html', context)
-
-
-def contact_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Contact page</h1>')
--- src/posts/forms.py
+++ src/posts/forms.py
@@ -15,14 +15,19 @@ class PostForm(forms.ModelForm):
 class BaseForm(forms.Form):
     title = forms.CharField(required=True,
                             widget=forms.TextInput(
-                                        attrs={'placeholder': 'Your title'})
+                                        attrs={'placeholder': 'Your title'}
+                                )
                             )
+
+    picture = forms.FileField(widget=forms.FileInput())
+
 
     description = forms.CharField(required=True,
                                   widget=forms.Textarea(
                                             attrs={'rows': 10,
                                                    'class': 'description',
-                                                   'cols': 100})
+                                                   'cols': 100}
+                                    )
                                   )
 
     contact_1 = forms.CharField(label='Contact 1', required=True,
@@ -47,23 +52,9 @@ class BaseForm(forms.Form):
 
 
 class LostForm(BaseForm):
+    award = forms.DecimalField(label='Award', widget=forms.NumberInput())
     pass
 
 
 class FoundForm(BaseForm):
     pass
-
-"""
-class RawPostForm(forms.Form):
-    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Your title'}))
-    description = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': 10, 'class': 'description', 'id': 'meep', 'cols': 100}))
-    award = forms.DecimalField(required=True, initial=0.00, label='Award')
-
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-
-        if len(title) <= 1:
-            raise forms.ValidationError('Title too short')
-
-        return title
-"""
