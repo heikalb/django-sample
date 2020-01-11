64062b36ae1f53470fa21d4b8f52a9d56e664f66
heikalb <heikal93@gmail.com>
2020-01-11 12:24:57 -0800

    fixed form field

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Post
 
 
+# For reference only (not used in webapp)
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -12,34 +13,36 @@ class PostForm(forms.ModelForm):
         ]
 
 
+# Basic form class to be extended
 class BaseForm(forms.Form):
     title = forms.CharField(required=True,
                             widget=forms.TextInput(
-                                        attrs={'placeholder': 'Your title'}
-                                )
+                                attrs={'placeholder': 'Your title'}
+                            )
                             )
 
     picture = forms.FileField(widget=forms.FileInput())
 
     description = forms.CharField(required=True,
                                   widget=forms.Textarea(
-                                            attrs={'rows': 10,
-                                                   'class': 'description',
-                                                   'cols': 100}
-                                    )
+                                      attrs={'rows': 10,
+                                             'class': 'description',
+                                             'cols': 100}
+                                  )
                                   )
 
-    contact_1 = forms.CharField(label='Contact 1', required=True,
-                                widget=forms.TextInput(
-                                            attrs={'placeholder': 'phone or email'}
-                                        )
-                                )
+    email = forms.EmailField(label='Contact 1', required=True,
+                            widget=forms.TextInput(
+                                attrs={'placeholder': 'phone or email'}
+                            )
+                            )
 
-    contact_2 = forms.CharField(label='Contact 2', required=True,
-                                widget=forms.TextInput(
-                                            attrs={'placeholder': 'alternative phone or email'}
-                                        )
-                                )
+    phone = forms.CharField(label='Contact 2', required=True,
+                            widget=forms.TextInput(
+                                attrs={
+                                    'placeholder': 'alternative phone or email'}
+                            )
+                            )
 
     def clean_title(self):
         title = self.cleaned_data.get('title')
@@ -50,10 +53,12 @@ class BaseForm(forms.Form):
         return title
 
 
+# Form for reporting lost pets
 class LostForm(BaseForm):
     award = forms.DecimalField(label='Award', widget=forms.NumberInput())
     pass
 
 
+# Form for reporting found pets
 class FoundForm(BaseForm):
     pass
