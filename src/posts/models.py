e4dd01e38bce972b8b44253738efcb8ad7668cb6
heikalb <heikal93@gmail.com>
2020-01-17 21:13:07 -0800

    prettified

--- src/posts/models.py
+++ src/posts/models.py
@@ -5,14 +5,20 @@ from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=80)
-    # picture = models.FileField(upload_to='uploads/', blank=True, null=True)
+
     picture = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
     description = models.TextField(null=True)
+
     email = models.EmailField(blank=True, null=True)
+
     phone = models.CharField(max_length=80, blank=True, null=True)
+
     date_posted = models.DateField(auto_now=True)
+
     award = models.DecimalField(max_digits=100, decimal_places=2, blank=True,
                                 null=True, default=0.00)
+
     password = models.CharField(default='password', max_length=100)
 
     def get_absolute_url(self):
