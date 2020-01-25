0e060589ab61f6dbef6abb4e97f51104a3a56e62
heikalb <heikal93@gmail.com>
2020-01-25 14:48:54 -0800

    False debug

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lipehe)we-_*cn#vbmqq)eraw^d#65_&w4%2enmev&73_-v&er'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['heikalb.pythonanywhere.com', '127.0.0.1']
 
