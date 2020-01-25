f530f9af3eafa266d503ac4ea3420cba1bf683ee
heikalb <heikal93@gmail.com>
2020-01-25 14:57:32 -0800

    Updating settings

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 # Project-specific settings
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
